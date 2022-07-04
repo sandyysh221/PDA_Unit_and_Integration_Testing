091e2a63a283d410f02956afba81378de5975cdc
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-04 15:09:22 +0100

    third test (subtraction) passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -27,4 +27,17 @@ describe("Calculator", () => {
     fireEvent.click(equalButton);
     expect(runningTotal.textContent).toEqual("5");
   });
+
+  it("should subtract 4 from 7 and get 3", () => {
+    const button4 = container.getByTestId("number4");
+    const button7 = container.getByTestId("number7");
+    const subtractButton = container.getByTestId("operator-subtract");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button7);
+    fireEvent.click(subtractButton);
+    fireEvent.click(button4);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("3");
+  });
 });
