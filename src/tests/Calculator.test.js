475bd4ac53d75c8ad037e6cd7240cf819439cfe4
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-07-04 15:00:23 +0100

    second test (adding) passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import Calculator from '../containers/Calculator';
-import {render, fireEvent} from '@testing-library/react';
+import React from "react";
+import Calculator from "../containers/Calculator";
+import { render, fireEvent } from "@testing-library/react";
 
-describe('Calculator', () => {
+describe("Calculator", () => {
   let container;
   beforeEach(() => {
-    container = render(<Calculator/>)
-  })
+    container = render(<Calculator />);
+  });
 
-  it('should change running total on number enter', () => {
-    const button4 = container.getByTestId('number4');
-    const runningTotal = container.getByTestId('running-total');
+  it("should change running total on number enter", () => {
+    const button4 = container.getByTestId("number4");
+    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button4);
-    expect(runningTotal.textContent).toEqual('4');
-  })
-})
+    expect(runningTotal.textContent).toEqual("4");
+  });
 
+  it("should add two numbers together", () => {
+    const button1 = container.getByTestId("number1");
+    const button2 = container.getByTestId("number2");
+    const addButton = container.getByTestId("operator-add");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button1);
+    fireEvent.click(addButton);
+    fireEvent.click(button2);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+});
